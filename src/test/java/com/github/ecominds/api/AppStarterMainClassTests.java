12706758bc71c95680ea8c86792f7df1a21d9e0b
Rajiv Kumar <ecominds7@gmail.com>
2023-01-20 20:39:46 +0530

    Initial commit

---
+++ src/test/java/com/github/ecominds/api/AppStarterMainClassTests.java
@@ -0,0 +1,19 @@
+/**
+ * @author	: Rajiv Kumar
+ * @project	: boot-rest-api
+ * @since	: 0.0.1
+ * @date	: 07-Jan-2023
+ */
+
+package com.github.ecominds.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppStarterMainClassTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
